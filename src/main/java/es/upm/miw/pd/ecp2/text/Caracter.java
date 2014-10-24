78073a642c9958935a27ad6f417b3b94bc93012a
kateMoscoso <katiemscs@gmail.com>
2014-10-24 12:32:44 +0200

    ejercicio Texto

---
+++ src/main/java/es/upm/miw/pd/ecp2/text/Caracter.java
@@ -0,0 +1,5 @@
+package es.upm.miw.pd.ecp2.text;
+
+public class Caracter {
+
+}
---
+++ src/main/java/es/upm/miw/pd/ecp2/text/FactoriaCaracter.java
@@ -0,0 +1,11 @@
+package es.upm.miw.pd.ecp2.text;
+
+import java.util.Map;
+
+
+public class FactoriaCaracter {
+	 private Map<String, Integer> caracteres;
+
+	    private int caracter;
+	    private static FactoryCaracter factoryReferences=null;
+}
